93076a37fc6166914c90e03f68ce88e2e96cd6e0
Tarun <gtarun14111@gmail.com>
2020-05-11 18:17:02 +0530

    Updates

---
+++ precache-manifest.b421b3a70b4830466bb891c66a0ccf4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fcc425f976081481989b54f357982f",
+    "url": "/projectbrain/index.html"
+  },
+  {
+    "revision": "6ff2abdea0c50f36a607",
+    "url": "/projectbrain/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "cbf1758fd174b1ace491",
+    "url": "/projectbrain/static/css/main.67ad0301.chunk.css"
+  },
+  {
+    "revision": "6ff2abdea0c50f36a607",
+    "url": "/projectbrain/static/js/2.8a9d984d.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/projectbrain/static/js/2.8a9d984d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf1758fd174b1ace491",
+    "url": "/projectbrain/static/js/main.68f00464.chunk.js"
+  },
+  {
+    "revision": "e4bf00c11a1e7cf266e5",
+    "url": "/projectbrain/static/js/runtime-main.fbcf297a.js"
+  }
+]);
